a184c48e52565a36578d998be90269ab7a22f026
fengyongwei <297115770@qq.com>
2017-10-20 09:35:35 +0800

    增加bjui的类

---
+++ src/main/java/org/mybatis/generator/custom/bjui/Bjui.java
@@ -0,0 +1,92 @@
+package org.mybatis.generator.custom.bjui;
+
+import java.io.Serializable;
+
+public class Bjui implements Serializable {
+	private static final long serialVersionUID = 486427439928047616L;
+
+	public static final int OK = 200;
+	public static final int ER = 300;
+	public static final int TO = 301;
+	public static final boolean CLOSE = true;
+	public static final boolean OPEN = false;
+
+	private int statusCode; // 必选。状态码
+	private String message; // 可选。信息内容。
+	private String tabid; // 可选。
+	private String dialogid; // 可选。
+	private String divid; // 可选。
+	private boolean closeCurrent; // 可选
+	private String forward; // 可选
+	private String forwardConfirm; // 可选
+
+	public int getStatusCode() {
+		return statusCode;
+	}
+
+	public void setStatusCode(int statusCode) {
+		this.statusCode = statusCode;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public String getTabid() {
+		return tabid;
+	}
+
+	public void setTabid(String tabid) {
+		this.tabid = tabid;
+	}
+
+	public String getDialogid() {
+		return dialogid;
+	}
+
+	public void setDialogid(String dialogid) {
+		this.dialogid = dialogid;
+	}
+
+	public String getDivid() {
+		return divid;
+	}
+
+	public void setDivid(String divid) {
+		this.divid = divid;
+	}
+
+	public boolean isCloseCurrent() {
+		return closeCurrent;
+	}
+
+	public void setCloseCurrent(boolean closeCurrent) {
+		this.closeCurrent = closeCurrent;
+	}
+
+	public String getForward() {
+		return forward;
+	}
+
+	public void setForward(String forward) {
+		this.forward = forward;
+	}
+
+	public String getForwardConfirm() {
+		return forwardConfirm;
+	}
+
+	public void setForwardConfirm(String forwardConfirm) {
+		this.forwardConfirm = forwardConfirm;
+	}
+
+	@Override
+	public String toString() {
+		return "Bjui [statusCode=" + statusCode + ", message=" + message + ", tabid=" + tabid + ", dialogid=" + dialogid + ", divid=" + divid + ", closeCurrent=" + closeCurrent + ", forward=" + forward + ", forwardConfirm=" + forwardConfirm + "]";
+	}
+
+}
